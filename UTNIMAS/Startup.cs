1a1fed7259282c72e1d7e8005f648d32b74fbbc9
Andres J. Jiménez Leandro <Andres.Leandro@forsyteit.com>
2019-09-20 11:15:33 -0600

    Adding the project

---
+++ UTNIMAS/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(UTNIMAS.Startup))]
+namespace UTNIMAS
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
