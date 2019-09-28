03afe3c6496406c7296b58dd79930490d033407a
Andres J. Jiménez Leandro <Andres.Leandro@forsyteit.com>
2019-09-28 17:30:17 -0600

    Conección a la DB

---
+++ UTNIMAS/Models/DataBase/ConexionBD.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Data.SqlClient;
+//Conexión a la Base de Datos
+namespace UTNIMAS
+{
+    public class ConexionBD
+    {
+        string conexion = "Data Source=DESKTOP-E38KCH7;Initial Catalog=UTNIMAS;Integrated Security=True";
+        public SqlConnection conectarBD = new SqlConnection();
+        //Constructor:
+        public ConexionBD()
+        {
+            conectarBD.ConnectionString = conexion;
+        }
+        //Abrir la Conexión de la DB:
+        public void abrir()
+        {
+            try
+            {
+                conectarBD.Open();
+                Console.WriteLine("Conexión Abierta");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error al Abrir la BD: " + e.Message);
+            }
+        }
+        //Cerrar la Conexión de la DB:
+        public void cerrar()
+        {
+            try
+            {
+                conectarBD.Close();
+                Console.WriteLine("Conexión Cerrada");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error al Cerrar la BD: " + e.Message);
+            }
+        }
+    }
+}
