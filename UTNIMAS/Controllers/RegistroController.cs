09f18313c643eae4f7a1453591fab02aa35ad316
Gabriel García <Gabriel.Garcia@forsyteit.com>
2019-10-11 09:59:00 -0600

    Missing views created, table updated

---
+++ UTNIMAS/Controllers/RegistroController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace UTNIMAS.Controllers
+{
+    public class RegistroController : Controller
+    {
+        // GET: Registro
+        public ActionResult RegistroEmpresas()
+        {
+            return View();
+        }
+        public ActionResult RegistroProductos()
+        {
+            return View();
+        }
+    }
+}
