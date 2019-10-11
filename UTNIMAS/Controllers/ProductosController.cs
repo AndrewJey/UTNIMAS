eff7ad2191c00e9a8b30b00e0964897552a38fb0
Gabriel García <Gabriel.Garcia@forsyteit.com>
2019-10-10 21:02:02 -0600

    views created,  logos and images changed

---
+++ UTNIMAS/Controllers/ProductosController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace UTNIMAS.Controllers
+{
+    public class ProductosController : Controller
+    {
+        // GET: Productos
+        public ActionResult Productos()
+        {
+            return View();
+        }
+    }
+}
