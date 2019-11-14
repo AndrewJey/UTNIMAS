8c71d3f2f615883440a9629915c2f9594a0f8fa8
Andres J. Jiménez Leandro <Andres.Leandro@forsyteit.com>
2019-11-14 10:47:37 -0600

    Linking hosting

--- UTNIMAS/Controllers/RegistroController.cs
+++ UTNIMAS/Controllers/RegistroController.cs
@@ -1,14 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
-using UTNIMAS.Models;
-using System.Web.UI;
-using System.Web.UI.WebControls;
-using System.Data.SqlClient;
-using System.Configuration;
 using System.Data;
+using System.Data.SqlClient;
+using System.Web.Mvc;
 
 namespace UTNIMAS.Controllers
 {
@@ -39,6 +32,23 @@ namespace UTNIMAS.Controllers
             con.abrir(); //Abre la BD                
             //CREAR COMANDO DE SQL
             SqlCommand cmd = new SqlCommand("Insert into dbo.PRODUCTS(PRODUCTOS_ID,NOMBRE_PRODUCTO,ID_PRECIO,NOMBRE_CONTACTO,DESCRIP_PRODUCTO,FOTO_PRODUCTO,EMPRESA_ID)values(@PRODUCTOS_ID,@NOMBRE_PRODUCTO,@ID_PRECIO,@DESCRIP_PRODUCTO,@FOTO_PRODUCTO,@EMPRESA_ID)", con.ConexDB());
+            //AGREGAR LOS PARAMETROS A LA BD
+            cmd.Parameters.Add("@Id", SqlDbType.Int).Value = Convert.ToInt32(1/*AGREGAR DATOS DEL FORM DE LA VISTA*/);
+            //cmd.Parameters.Add("@Name", SqlDbType.VarChar, 30).Value = Console.ReadLine();
+            //EJECUTA LOS COMANDOS SQL
+            //cmd.ExecuteNonQuery();
+            //Cerrar la Conexon con la DB
+            con.cerrar();
+            //Mostrar Vista
+            return View();
+        }
+        public ActionResult RegistroPrecios()
+        {
+            ConexionBD con = new ConexionBD(); //Crea la instancia de la conexion
+            con.ConexDB(); //Conecta la BD
+            con.abrir(); //Abre la BD                
+            //CREAR COMANDO DE SQL
+            SqlCommand cmd = new SqlCommand("Insert into dbo.PRECIOS(PRECIOS_ID,PRECIO_UNIDAD,PRECIO_PAQUETE,PRECIO_SERVICIO,PRECIO_MAYOREO,PRECIO_ESPECIAL)values(@PRECIOS_ID,@PRECIO_UNIDAD,@PRECIO_PAQUETE,@PRECIO_SERVICIO,@PRECIO_MAYOREO,@PRECIO_ESPECIAL)", con.ConexDB());
             //AGREGAR LOS PARAMETROS A LA BD
             cmd.Parameters.Add("@Id", SqlDbType.Int).Value = Convert.ToInt32(1/*AGREGAR DATOS DEL FORM DE LA VISTA*/);
             //cmd.Parameters.Add("@Name", SqlDbType.VarChar, 30).Value = Console.ReadLine();
