db8b27981dab4a945e9231099a26a2fc4613dd11
Andres J. Jiménez Leandro <Andres.Leandro@forsyteit.com>
2019-12-06 01:55:55 -0600

    Cambios Generales con Datos de Gabriel

--- UTNIMAS/Controllers/RegistroController.cs
+++ UTNIMAS/Controllers/RegistroController.cs
@@ -2,6 +2,7 @@
 using System.Data;
 using System.Data.SqlClient;
 using System.Web.Mvc;
+using UTNIMAS.Models;
 
 namespace UTNIMAS.Controllers
 {
--- UTNIMAS/Models/DataBase/ConexionBD.cs
+++ UTNIMAS/Models/DataBase/ConexionBD.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Data.SqlClient;
 //Conexión a la Base de Datos
-namespace UTNIMAS
+namespace UTNIMAS.Models
 {
     public class ConexionBD
     {
